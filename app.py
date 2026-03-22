ad25d60f25f6e1550c640cb65b8d87e4c6db0357
Zafar Aftab <105731938+zafaraftab1@users.noreply.github.com>
2026-03-22 16:02:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,6 +73,13 @@ def ladderLength(beginWord, endWord, wordList):
 --------------------------
 
 #code for fibbo
+def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+fib(10)
 #
 
 
